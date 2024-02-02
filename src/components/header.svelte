104ed8e10846d2465b1320d522153055a2a3f02f
M1nfr3y <turrinialessio5@gmail.com>
2024-02-02 17:50:37 +0100

    primo commit

---
+++ src/components/header.svelte
@@ -0,0 +1,60 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <li class="nav-item me-3 me-lg-0">
+        <a class="nav-link text-white" href="#"><i class="fab fa-twitter"></i></a>
+    </li>
+    <ul class="navbar-nav d-flex flex-row me-1">
+      <li class="nav-item me-3 me-lg-0">
+          <a class="nav-link text-white" href="#"><i class="fas fa-shopping-cart"></i></a>
+      </li>
+      <li class="nav-item me-3 me-lg-0">
+          <a class="nav-link text-white" href="#"><i class="fab fa-twitter"></i></a>
+      </li>
+      <button type="button" class="btn btn-light" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
+        <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1"></path></button> 
+    
+  </ul>
+    </div>
+  </div>
+</nav>
+
+<style>
+  .navbar{
+    background-color: #403174;
+  }
+
+  svg{
+    color:aliceblue ;
+  }
+  .btn{
+    background-color: #403174;
+  }
+</style>
