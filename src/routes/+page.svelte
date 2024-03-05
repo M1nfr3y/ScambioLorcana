a85067252081b7a820fa1c5db1e123b73f2d1c3d
aub3men <aub3men@posteitaliane.it>
2024-03-05 14:56:46 +0100

    prova componente ListGroup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,14 @@
   import Card from "../components/Card.svelte";
   import Footer from "../components/UI/Footer.svelte";
   import Carosello from "../components/UI/Carosello.svelte";
+  import ListGroup from "../components/UI/ListGroup.svelte";
 
   let cards = ["Titolo 1", "Titolo 2", "Titolo 3", "Titolo 4"];
 </script>
 
 <Header ></Header>
 <Carosello ></Carosello>
+<ListGroup></ListGroup>
 <Footer></Footer>
 
 <style>
